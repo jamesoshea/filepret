ebdc3cec32ed568fe9e44cdf4556952dc1a898d8
James O'Shea <j.oshea@holidaypirates.com>
2019-07-15 20:15:39 +0200

    feat: show formatted file with lovely syntax highlighting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  const uploadPath = `${process.env.REACT_APP_BASE_URL}/upload`
-  return (
-    <div className="App">
-    <h1>most-host</h1>
-    <p>Welcome to cool app</p>
-    <form method="post" action={uploadPath} encType="multipart/form-data">
-      <input type="file" name="fileUploaded" />
-      <input type="submit" />
-    </form>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      formattedFile: ""
+    };
+    this.uploadFile = e => {
+      const files = Array.from(e.target.files);
+      const formData = new FormData();
+
+      files.forEach((file, i) => {
+        formData.append(i, file);
+      });
+      axios
+        .post(`${process.env.REACT_APP_BASE_URL}/upload`, formData, {
+          headers: {
+            Accept: "text/plain",
+            Encoding: "multipart/form-data"
+          }
+        })
+        .then(res => {
+          this.setState({
+            formattedFile: res.data
+          });
+        });
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>most-host</h1>
+        <p>Welcome to cool app</p>
+        <form onSubmit={() => false}>
+          <input type="file" name="fileUploaded" onChange={this.uploadFile} />
+        </form>
+        {this.state.formattedFile && (
+          <div>
+            <h1>voici le file, wow</h1>
+            <SyntaxHighlighter language="javascript" style={docco}>
+              {this.state.formattedFile}
+            </SyntaxHighlighter>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -4,34 +4,43 @@ const path = require("path");
 const prettier = require("prettier");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.use(fileUpload());
+app.use(cors());
 
 app.post("/api/upload", function(req, res) {
   if (!req.files || Object.keys(req.files).length == 0) {
     return res.status(400).send("No files were uploaded.");
   }
-  const file = req.files.fileUploaded;
+  const file = req.files[0];
   const currentTime = Date.now();
-  const fileName = `./files/${file.md5}-${currentTime}.js`
-  file.mv(fileName, (err) => {
+  const fileName = `./files/${file.md5}-${currentTime}.js`;
+  file.mv(fileName, err => {
     if (err) return res.status(500).send(err);
     const unformattedFile = fs.readFileSync(fileName).toString();
     const formattedFile = prettier.format(unformattedFile, {
       semi: false,
       parser: "babel"
     });
-    fs.writeFileSync(`./files/${file.md5}-${currentTime}-formatted.js`, formattedFile);
-    res.sendFile(path.join(__dirname, `./files/${file.md5}-${currentTime}-formatted.js`), {}, (err) => {
+    fs.writeFileSync(
+      `./files/${file.md5}-${currentTime}-formatted.js`,
+      formattedFile
+    );
+    res.sendFile(
+      path.join(__dirname, `./files/${file.md5}-${currentTime}-formatted.js`),
+      {},
+      err => {
         if (err) {
-          console.log(err)
+          console.log(err);
         } else {
-          console.log('File sent')
+          console.log("File sent");
         }
-      })
+      }
+    );
   });
 });
 
