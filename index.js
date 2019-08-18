abf793191c2504b9cb39b00bb81c536ca8d35141
James O'Shea <j.oshea@holidaypirates.com>
2019-08-18 20:26:32 +0200

    feat: create file upload nanoservice

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const path = require("path");
 const prettier = require("prettier");
+
+const { version } = require("./package.json");
 const app = express();
 
 const { S3PutObjectPromisified } = require("./util/promisified-functions");
 
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, "../build")));
 app.use(fileUpload());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send(`Prêt-à-formatter upload version ${version} running`);
+});
 
 app.post("/upload", async (req, res, next) => {
   try {
@@ -38,9 +42,11 @@ app.post("/upload", async (req, res, next) => {
   }
 });
 
-app.use((error, req, res) => {
+// eslint-disable-next-line
+app.use((error, req, res, next) => {
   console.log(error);
   res.status(500).json({ message: error.message });
+  next();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
